89a5e51c07b462c83a9b73030c43e606d4bf7fc3
Charles Bonmann <clbonmann@gmail.com>
2026-01-07 13:52:21 +0400

    Remove dependency on logging config in alembic.env.py
    Comment out logging configuration in alembic.env.py.

--- alembic/env.py
+++ alembic/env.py
@@ -13,8 +13,10 @@ import os
 
 config = context.config
 
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+# Railway/MVP: não dependa de logging config no alembic.ini
+# Se quiser logs, a gente configura depois.
+# if config.config_file_name is not None:
+#     fileConfig(config.config_file_name)
 
 target_metadata = Base.metadata
 
