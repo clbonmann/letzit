2aa0bafcc9154e3455316ced40cad11e2600e71a
Charles Bonmann <clbonmann@gmail.com>
2026-01-07 10:35:55 +0400

    Refactor Celery app initialization
    Refactor Celery app initialization to use settings directly.

--- app/tasks.py
+++ app/tasks.py
@@ -1,11 +1,9 @@
 from celery import Celery
 from app.settings import settings
 
-broker = settings.CELERY_BROKER_URL or settings.REDIS_URL
-backend = settings.CELERY_RESULT_BACKEND or settings.REDIS_URL
+celery_app = Celery(
+    "letzit",
+    broker=settings.celery_broker,
+    backend=settings.celery_backend,
+)
 
-celery_app = Celery("mvp", broker=broker, backend=backend)
-
-@celery_app.task
-def ping():
-    return "pong"
