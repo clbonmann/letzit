45d18d40d268dc4ab9d3025087a29b25de0d0c79
Charles Bonmann <clbonmann@gmail.com>
2026-01-08 21:34:49 +0400

    Update offers.py

--- app/routes/offers.py
+++ app/routes/offers.py
@@ -39,7 +39,7 @@ async def accept_offer(
         {"oid": offer_id, "uid": user_id},
     )).first()
     if not target:
-        return AcceptOfferResponse(status="OFFER_NOT_ELIGIBLE",  user_id=user_id, offer_id=offer_id)
+        return AcceptOfferResponse(status="OFFER_NOT_ELIGIBLE", offer_id=offer_id, user_id=user_id)
 
     # 3) lock offer row (this is the FCFS part)
     offer = (await db.execute(
