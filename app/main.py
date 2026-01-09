8490cad44aabdb242a333da4aa873d7d3673cc72
Charles Bonmann <clbonmann@gmail.com>
2026-01-09 13:55:37 +0400

    Update main.py
    Add offers list

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ from app.routes.staff_offers_create import router as staff_offers_create_router
 from app.routes.home import router as home_router
 from app.routes.offers_public import router as offers_public_router
 from app.routes.staff_jobs import router as staff_jobs_router
+from app.routes.staff_offers_list import router as staff_offers_list_router
 
 app = FastAPI(
     title=settings.APP_NAME,
@@ -31,6 +32,7 @@ app.include_router(staff_offers_create_router)
 app.include_router(home_router)
 app.include_router(offers_public_router)
 app.include_router(staff_jobs_router)
+app.include_router(staff_offers_list_router)
 
 @app.get("/health")
 def health():
