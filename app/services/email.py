a6caf0fa3069df5c7b007a01ce3e23d76a8323c2
clbonmann <clbonmann@gmail.com>
2026-01-28 10:20:17 +0400

    New resend config

--- app/services/email.py
+++ app/services/email.py
@@ -1,13 +1,16 @@
-import certifi
 import os
-
-# --- CORREÇÃO DE SSL PARA WINDOWS ---
-# Isso força o sistema a usar o certificado correto instalado pelo pip
-os.environ["SSL_CERT_FILE"] = certifi.where()
-
+import certifi
 import resend
 from app.settings import settings
 from app.emails.staff_invite import build_staff_invite_email
+
+real_cert_path = certifi.where()
+print(f"🔒 Certificado SSL carregado de: {real_cert_path}")
+
+# 2. Força TODAS as variáveis de ambiente que o Python/Requests usam
+# O erro acontece porque o 'REQUESTS_CA_BUNDLE' provavelmente está apontando pro lugar errado
+os.environ["SSL_CERT_FILE"] = real_cert_path
+os.environ["REQUESTS_CA_BUNDLE"] = real_cert_path
 
 # Configure a chave (adicione RESEND_API_KEY no .env e settings.py)
 resend.api_key = settings.RESEND_API_KEY
@@ -22,15 +25,16 @@ async def send_invite_email(email: str, name: str, token: str, restaurant_name: 
         staff_email=clean_email,
         token=token
     )
-    
+    params: resend.Emails.SendParams = {
+        "from": "LetzIT Team <onboard@letzit.com.br>", # Tem que ser o domínio verificado
+        "to": [clean_email],
+        "subject": subject,
+        "html": html_content
+    }
+
     try:
-        r = resend.Emails.send({
-            "from": "LetzIT Team <onboard@letzit.com.br>", # Tem que ser o domínio verificado
-            "to": [clean_email],
-            "subject": subject,
-            "html": html_content
-        })
-        print(f"✅ Email enviado via Resend ID: {r['id']}")
+        email = resend.Emails.send(params)
+        print(f"✅ Email enviado via Resend ID: {email['id']}")
         return True
     except Exception as e:
         print(f"❌ Erro Resend: {e}")
